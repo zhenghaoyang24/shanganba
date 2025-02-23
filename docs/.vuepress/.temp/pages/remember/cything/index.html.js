f9f60cd5e031a1d1249eddc5294d552490115ee7
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-02-23 21:19:52 +0800

    docs(idiom):add idiom in thing

--- docs/.vuepress/.temp/pages/remember/cything/index.html.js
+++ docs/.vuepress/.temp/pages/remember/cything/index.html.js
@@ -1,5 +1,5 @@
 import comp from "G:/200-Project/shanganba/docs/.vuepress/.temp/pages/remember/cything/index.html.vue"
-const data = JSON.parse("{\"path\":\"/remember/cything/\",\"title\":\"事物发展\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"事物发展\",\"createTime\":\"2025/02/22 14:29:32\",\"permalink\":\"/remember/cything/\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":3.85,\"words\":1154},\"filePathRelative\":\"notes/remember/高频成语/事物发展.md\"}")
+const data = JSON.parse("{\"path\":\"/remember/cything/\",\"title\":\"事物发展\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"事物发展\",\"createTime\":\"2025/02/22 14:29:32\",\"permalink\":\"/remember/cything/\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":3.98,\"words\":1193},\"filePathRelative\":\"notes/remember/高频成语/事物发展.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
