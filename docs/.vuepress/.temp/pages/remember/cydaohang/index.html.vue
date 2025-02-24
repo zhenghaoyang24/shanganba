9c81428283828d1447611edd7b9839411d21d764
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-02-24 23:00:05 +0800

    docs(idiom):add idiom in sayanddo

--- docs/.vuepress/.temp/pages/remember/cydaohang/index.html.vue
+++ docs/.vuepress/.temp/pages/remember/cydaohang/index.html.vue
@@ -2,11 +2,11 @@
 <h2 id="a" tabindex="-1"><a class="header-anchor" href="#a"><span>a</span></a></h2>
 <p><RouteLink to="/remember/cyinnovate/#%E6%8C%89%E5%9B%BE%E7%B4%A2%E9%AA%A5">按图索骥</RouteLink></p>
 <h2 id="b" tabindex="-1"><a class="header-anchor" href="#b"><span>b</span></a></h2>
-<p><RouteLink to="/remember/cyinnovate/#%E6%8A%B1%E6%AE%8B%E5%AE%88%E7%BC%BA">抱残守缺</RouteLink> <RouteLink to="/remember/cyinnovate/#%E5%88%AB%E5%87%BA%E5%BF%83%E8%A3%81">别出心裁</RouteLink> <RouteLink to="/remember/cyculture/#%E4%B8%8D%E7%BB%9D%E5%A6%82%E7%BC%95">不绝如缕</RouteLink>  <RouteLink to="/remember/cyinnovate/#%E5%88%AB%E5%BC%80%E7%94%9F%E9%9D%A2">别开生面</RouteLink> <RouteLink to="/remember/cything/#%E6%B3%A2%E6%BE%9C%E5%A3%AE%E9%98%94">波澜壮阔</RouteLink> <RouteLink to="/remember/cything/#%E9%98%AA%E4%B8%8A%E8%B5%B0%E4%B8%B8">阪上走丸</RouteLink> <RouteLink to="/remember/cyinnovate/#%E6%A0%87%E6%96%B0%E7%AB%8B%E5%BC%82">标新立异</RouteLink></p>
+<p><RouteLink to="/remember/cyinnovate/#%E6%8A%B1%E6%AE%8B%E5%AE%88%E7%BC%BA">抱残守缺</RouteLink> <RouteLink to="/remember/cyinnovate/#%E5%88%AB%E5%87%BA%E5%BF%83%E8%A3%81">别出心裁</RouteLink> <RouteLink to="/remember/sayanddo/#%E6%8D%95%E9%A3%8E%E6%8D%89%E5%BD%B1">捕风捉影</RouteLink> <RouteLink to="/remember/cyculture/#%E4%B8%8D%E7%BB%9D%E5%A6%82%E7%BC%95">不绝如缕</RouteLink>  <RouteLink to="/remember/cyinnovate/#%E5%88%AB%E5%BC%80%E7%94%9F%E9%9D%A2">别开生面</RouteLink> <RouteLink to="/remember/cything/#%E6%B3%A2%E6%BE%9C%E5%A3%AE%E9%98%94">波澜壮阔</RouteLink> <RouteLink to="/remember/cything/#%E9%98%AA%E4%B8%8A%E8%B5%B0%E4%B8%B8">阪上走丸</RouteLink> <RouteLink to="/remember/cyinnovate/#%E6%A0%87%E6%96%B0%E7%AB%8B%E5%BC%82">标新立异</RouteLink></p>
 <h2 id="c" tabindex="-1"><a class="header-anchor" href="#c"><span>c</span></a></h2>
 <p><RouteLink to="/remember/cything/#%E5%B1%82%E5%87%BA%E4%B8%8D%E7%A9%B7">层出不穷</RouteLink> <RouteLink to="/remember/cyinnovate/#%E9%99%88%E9%99%88%E7%9B%B8%E5%9B%A0">陈陈相因</RouteLink></p>
 <h2 id="d" tabindex="-1"><a class="header-anchor" href="#d"><span>d</span></a></h2>
-<p><RouteLink to="/remember/cyinnovate/#%E7%8B%AC%E6%A0%91%E4%B8%80%E5%B8%9C">独树一帜</RouteLink></p>
+<p><RouteLink to="/remember/sayanddo/#%E5%A4%A7%E8%80%8C%E5%8C%96%E4%B9%8B">大而化之</RouteLink> <RouteLink to="/remember/sayanddo/#%E6%BB%B4%E6%B0%B4%E4%B8%8D%E6%BC%8F">滴水不漏</RouteLink> <RouteLink to="/remember/cyinnovate/#%E7%8B%AC%E6%A0%91%E4%B8%80%E5%B8%9C">独树一帜</RouteLink></p>
 <h2 id="f" tabindex="-1"><a class="header-anchor" href="#f"><span>f</span></a></h2>
 <p><RouteLink to="/remember/cything/#%E9%A3%8E%E8%B5%B7%E4%BA%91%E6%B6%8C">风起云涌</RouteLink> <RouteLink to="/remember/cything/#%E6%96%B9%E5%85%B4%E6%9C%AA%E8%89%BE">方兴未艾</RouteLink> <RouteLink to="/remember/cysituation/#%E9%A3%8E%E9%9B%A8%E9%A3%98%E6%91%87">风雨飘摇</RouteLink></p>
 <h2 id="g" tabindex="-1"><a class="header-anchor" href="#g"><span>g</span></a></h2>
@@ -20,7 +20,7 @@
 <h2 id="n" tabindex="-1"><a class="header-anchor" href="#n"><span>n</span></a></h2>
 <p><RouteLink to="/remember/cysituation/#%E9%9A%BE%E4%BB%A5%E4%B8%BA%E7%BB%A7">难以为继</RouteLink></p>
 <h2 id="q" tabindex="-1"><a class="header-anchor" href="#q"><span>q</span></a></h2>
-<p><RouteLink to="/remember/cysituation/#%E9%AA%91%E8%99%8E%E9%9A%BE%E4%B8%8B">骑虎难下</RouteLink></p>
+<p><RouteLink to="/remember/cysituation/#%E9%AA%91%E8%99%8E%E9%9A%BE%E4%B8%8B">骑虎难下</RouteLink> <RouteLink to="/remember/sayanddo/#%E8%BD%BB%E6%8F%8F%E6%B7%A1%E5%86%99">轻描淡写</RouteLink></p>
 <h2 id="r" tabindex="-1"><a class="header-anchor" href="#r"><span>r</span></a></h2>
 <p><RouteLink to="/remember/cything/#%E5%A6%82%E7%81%AB%E5%A6%82%E8%8D%BC">如火如荼</RouteLink> <RouteLink to="/remember/cything/#%E6%97%A5%E6%B8%90%E5%BC%8F%E5%BE%AE">日渐式微</RouteLink> <RouteLink to="/remember/cything/#%E5%A6%82%E6%97%A5%E4%B8%AD%E5%A4%A9">如日中天</RouteLink> <RouteLink to="/remember/cything/#%E6%97%A5%E6%96%B0%E6%9C%88%E5%BC%82">日新月异</RouteLink></p>
 <h2 id="s" tabindex="-1"><a class="header-anchor" href="#s"><span>s</span></a></h2>
@@ -32,7 +32,9 @@
 <h2 id="x" tabindex="-1"><a class="header-anchor" href="#x"><span>x</span></a></h2>
 <p><RouteLink to="/remember/cyinnovate/#%E5%BE%AA%E8%A7%84%E8%B9%88%E7%9F%A9">循规蹈矩</RouteLink> <RouteLink to="/remember/cyculture/#%E8%96%AA%E7%81%AB%E7%9B%B8%E4%BC%A0">薪火相传</RouteLink></p>
 <h2 id="y" tabindex="-1"><a class="header-anchor" href="#y"><span>y</span></a></h2>
-<p><RouteLink to="/remember/cything/#%E9%9B%A8%E5%90%8E%E6%98%A5%E7%AC%8B">雨后春笋</RouteLink> <RouteLink to="/remember/cything/#%E5%BC%82%E5%86%9B%E7%AA%81%E8%B5%B7">异军突起</RouteLink> <RouteLink to="/remember/cyculture/#%E4%B8%80%E8%84%89%E7%9B%B8%E6%89%BF">一脉相承</RouteLink> <RouteLink to="/remember/cything/#%E5%81%83%E6%97%97%E6%81%AF%E9%BC%93">偃旗息鼓</RouteLink> <RouteLink to="/remember/cything/#%E4%B8%80%E6%97%A5%E5%8D%83%E9%87%8C">一日千里</RouteLink> <RouteLink to="/remember/cyculture/#%E6%BA%90%E8%BF%9C%E6%B5%81%E9%95%BF">源远流长</RouteLink> <RouteLink to="/remember/cything/#%E5%BA%94%E8%BF%90%E8%80%8C%E7%94%9F">应运而生</RouteLink></p>
+<p><RouteLink to="/remember/sayanddo/#%E6%9C%89%E7%9A%84%E6%94%BE%E7%9F%A2">有的放矢</RouteLink> <RouteLink to="/remember/cything/#%E9%9B%A8%E5%90%8E%E6%98%A5%E7%AC%8B">雨后春笋</RouteLink> <RouteLink to="/remember/cything/#%E5%BC%82%E5%86%9B%E7%AA%81%E8%B5%B7">异军突起</RouteLink> <RouteLink to="/remember/cyculture/#%E4%B8%80%E8%84%89%E7%9B%B8%E6%89%BF">一脉相承</RouteLink> <RouteLink to="/remember/cything/#%E5%81%83%E6%97%97%E6%81%AF%E9%BC%93">偃旗息鼓</RouteLink> <RouteLink to="/remember/cything/#%E4%B8%80%E6%97%A5%E5%8D%83%E9%87%8C">一日千里</RouteLink> <RouteLink to="/remember/cyculture/#%E6%BA%90%E8%BF%9C%E6%B5%81%E9%95%BF">源远流长</RouteLink> <RouteLink to="/remember/cything/#%E5%BA%94%E8%BF%90%E8%80%8C%E7%94%9F">应运而生</RouteLink></p>
+<h2 id="z" tabindex="-1"><a class="header-anchor" href="#z"><span>z</span></a></h2>
+<p><RouteLink to="/remember/sayanddo/#%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1">走马观花</RouteLink></p>
 </div></template>
 
 
