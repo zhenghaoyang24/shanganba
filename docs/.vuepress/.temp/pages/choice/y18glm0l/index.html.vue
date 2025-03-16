2324e2322c164ac037852c90908d57276a7456fc
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-03-16 22:46:06 +0800

    docs: add read doc

--- docs/.vuepress/.temp/pages/choice/y18glm0l/index.html.vue
+++ docs/.vuepress/.temp/pages/choice/y18glm0l/index.html.vue
@@ -1,4 +1,6 @@
 <template><div><p>片段阅读的考法较多，有主旨概括题、意图推断题、标题添加题、细节理解题、词句理解题。</p>
-</div></template>
+<h2 id="主旨概括题" tabindex="-1"><a class="header-anchor" href="#主旨概括题"><span>主旨概括题</span></a></h2>
+<p>主旨概括题即 给出一片段，要求总结概括文段内容、总结主旨。需抓住文段的<strong>主题词</strong>、<strong>关联词</strong>与<strong>行文脉络</strong>进行分析，概括主旨。</p>
+<MarkMap id="markmap-9" content="eJxtUctu2lAQ3fMVlthZ4geya/+ERbtpaaJ215V5BQgPo4YA5SVoEpM2wS5KimzM42NyZ3y96i/0XNtpUBTpLuZxZs6cczOZTCqX/fwhlz05SmnaSfbTkfY++/HLOyRfj49zT5nCpdKacH3u3bBV4Pod8rRG5XuZ70inqbK0Jrd3fNZ/LmaSivDq1G5x3wkrzaDgxeDQ+Cb3w0NwXHkVTMMpT0aH4KCz5G6F7T+061FlzePrYFAK/HPA/m4atDPwXi7Zl6WV5/FM+CtsIPMXj2pvgOZaUxa3bx+NwiGEhw+YRop6ssBbUbWH0WSOTIuuutFc1KHtuXAX3JuK3RyjQIRGjes/w9s+ra3Yt0cjz2abzC6Vl2zMcTQw4cxLSJTH4Y9+rJGnLu2L0hmjos6M9V4NaPgQ+BdUXtHygjYGWsLtkOviic0AbgSt3zS6Ca0umddqMVYtLoOZHVPpOmSSPaD2LYT8J9T1pxvkTHHJUi3wR9EZHjfthL16ypM2VCKI3C4pH6C+sUYFJ6EFTXJegO3QKueXYeUMQdizQaMqxQZ/d5QNJVN4NQTkeMGiGywQq1PhYsylWKIT41msjZGxrxBCuyqZDr5OuGtdBxWd3r/A6jrQ6BmNcDwRrvpnNlrCq1J+kAj+B4N82Uc="></MarkMap></div></template>
 
 
