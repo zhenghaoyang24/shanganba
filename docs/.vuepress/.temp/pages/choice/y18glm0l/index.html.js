2a895bd6280b013c6fe2eb889b4a912f46060c5b
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-03-17 11:39:52 +0800

    docs: add yanyulijie

--- docs/.vuepress/.temp/pages/choice/y18glm0l/index.html.js
+++ docs/.vuepress/.temp/pages/choice/y18glm0l/index.html.js
@@ -1,5 +1,5 @@
 import comp from "G:/200-Project/shanganba/docs/.vuepress/.temp/pages/choice/y18glm0l/index.html.vue"
-const data = JSON.parse("{\"path\":\"/choice/y18glm0l/\",\"title\":\"片段阅读\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"片段阅读\",\"createTime\":\"2025/02/18 22:36:22\",\"permalink\":\"/choice/y18glm0l/\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":1,\"words\":301},\"filePathRelative\":\"notes/choice/2.言语理解/2.片段阅读.md\"}")
+const data = JSON.parse("{\"path\":\"/choice/y18glm0l/\",\"title\":\"片段阅读\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"片段阅读\",\"createTime\":\"2025/02/18 22:36:22\",\"permalink\":\"/choice/y18glm0l/\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":2.26,\"words\":678},\"filePathRelative\":\"notes/choice/2.言语理解/2.片段阅读.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/.temp/pages/choice/y18glm0l/index.html.vue
+++ docs/.vuepress/.temp/pages/choice/y18glm0l/index.html.vue
@@ -1,6 +1,17 @@
 <template><div><p>片段阅读的考法较多，有主旨概括题、意图推断题、标题添加题、细节理解题、词句理解题。</p>
 <h2 id="主旨概括题" tabindex="-1"><a class="header-anchor" href="#主旨概括题"><span>主旨概括题</span></a></h2>
 <p>主旨概括题即 给出一片段，要求总结概括文段内容、总结主旨。需抓住文段的<strong>主题词</strong>、<strong>关联词</strong>与<strong>行文脉络</strong>进行分析，概括主旨。</p>
-<MarkMap id="markmap-9" content="eJxtUctu2lAQ3fMVlthZ4geya/+ERbtpaaJ215V5BQgPo4YA5SVoEpM2wS5KimzM42NyZ3y96i/0XNtpUBTpLuZxZs6cczOZTCqX/fwhlz05SmnaSfbTkfY++/HLOyRfj49zT5nCpdKacH3u3bBV4Pod8rRG5XuZ70inqbK0Jrd3fNZ/LmaSivDq1G5x3wkrzaDgxeDQ+Cb3w0NwXHkVTMMpT0aH4KCz5G6F7T+061FlzePrYFAK/HPA/m4atDPwXi7Zl6WV5/FM+CtsIPMXj2pvgOZaUxa3bx+NwiGEhw+YRop6ssBbUbWH0WSOTIuuutFc1KHtuXAX3JuK3RyjQIRGjes/w9s+ra3Yt0cjz2abzC6Vl2zMcTQw4cxLSJTH4Y9+rJGnLu2L0hmjos6M9V4NaPgQ+BdUXtHygjYGWsLtkOviic0AbgSt3zS6Ca0umddqMVYtLoOZHVPpOmSSPaD2LYT8J9T1pxvkTHHJUi3wR9EZHjfthL16ypM2VCKI3C4pH6C+sUYFJ6EFTXJegO3QKueXYeUMQdizQaMqxQZ/d5QNJVN4NQTkeMGiGywQq1PhYsylWKIT41msjZGxrxBCuyqZDr5OuGtdBxWd3r/A6jrQ6BmNcDwRrvpnNlrCq1J+kAj+B4N82Uc="></MarkMap></div></template>
+<MarkMap id="markmap-9" content="eJxtUctu2kAU3fMVlthZ4geya/+ERbtpaaJ215V5BRweRg2xqXkImsSkTbCLkiIb8/iYzL2eWfUXesfjNCiKNIv7OPfec84UCoVcqfj5Q6l4cpTTtJPipyPtffHjl3eUfD0+Lj1lEpfLayyM0blBr4KtO8rzGtTvebnPg47M8hrf3uHZ4LlYyCosakGvi4NANDpJJVJgYXzj++EhWFVeBcNwipPRITjpL9FuoP8Hdg401ji+TtxaEp8T7O+mDTuD3ssl+zr3yjiesXhFG8D6hSPzDaHR7PDq9u2jUTmE4PCBpimlerYgWkHTodFsDiwPrux0Lu3A9pyFC3SmbDenUUIIw8TWT3E7gLWnfHs0ymj1wLKhvkRjTqQJI2ZRdkR6LH4MlEachrCv8mBMFUlT6b1yYfiQxBdQX8HyAjYGtVjYhzCkxzYuuZF0f8PoRng2WNdyMa1aXCYzX53SdZIJvgu9WxLy/6CuP3HgM3mL18wkHqU0Iuz42fXmKU56pJKC1O2a9IHUt9dU4fMKuU0S+fxSNM4oEI5P22Wl2sbvgVRfs1hkUgBBlCzsZEGxZEjmqRNyecpMzZJDCqnsJP6wa4IV0I+xcK3rdApO719gdZ3Q1DPaYjxhofxeNLosakLZzXT+A6IU0tk="></MarkMap><h2 id="意图推断题" tabindex="-1"><a class="header-anchor" href="#意图推断题"><span>意图推断题</span></a></h2>
+<p>意图推断即文章<strong>意在强调什么</strong>。</p>
+<MarkMap id="markmap-16" content="eJxNkMtOg0AUhvc8BUl3JLxA34aFbrS20Z2roaWlhJaaUKlSNKLR4gWoVhMKqA/TubHiFTwTonF3zsw3/3fmqKoqdbTjg47Wa0uy3NOO2vK+dniyB81pt9v57QQntWRqzMjyizoR9WKp1ZJJlvGVwV4LSZUr5PNvkww3XJ/zdLpDOs6K6vaiqXk4oZ7JDYsVAYutupw0KcTM4Zw934joICJlztcDXCC8HQPDwoQkPuTQRURm9zvUBxHwNPmAh8xZV4sEFDhzSLplsSdil+80sMgs5YPPClnUflQUvrojo03DMt9oWEWpy3F1dY2zKaAUOaAkul+XltCIz6UujxA9H+LcBi1wLLCbFopKd9nZ6G8cnMWgpahghUvGl2weNbWY6C38f8XNJ7GMhz61X4TpB03T7sg="></MarkMap><h2 id="标题添加题" tabindex="-1"><a class="header-anchor" href="#标题添加题"><span>标题添加题</span></a></h2>
+<p>标题添加题即给一段文字，要求选择最合适的标题，标题中需是<strong>高度概括且含有主题词</strong>。</p>
+<MarkMap id="markmap-23" content="eJxVzk8KgkAUBvD9nGLAZXgBb+OiNmVK7VqFFWJGm9DcpQWBkP9CSDPpMvqcuUWjZdTy+96P9x7P80gSJ0NJVASEsSKOBTwQR9M+CzNZlrrUOMRhcDR6tOGWV2sHcRwmQUCCO1ga4jGd62B4LNZq1qvCrPYtVlE3a2GYgL39g23DHIndHphJWezgrIJxaThYEd3ndZx/+XtWhQ8S+q0wT2wbpCkJot/j5Bl1OxcrWB6Ifq0Kr0xZu/m88wLGzodS"></MarkMap><h2 id="细节理解题" tabindex="-1"><a class="header-anchor" href="#细节理解题"><span>细节理解题</span></a></h2>
+<p>细节理解题中的各选项一般对应文段中的某几句。一般含有<strong>无中生有</strong>、<strong>偷换时态</strong>、<strong>偷换数量</strong>、<strong>偷换逻辑</strong>、<strong>偷换概念</strong>、
+<strong>偷换语气</strong>。</p>
+<p>需注意题干为<strong>选非还是选是</strong>，先看选项，再看文段。</p>
+<h2 id="词句理解题" tabindex="-1"><a class="header-anchor" href="#词句理解题"><span>词句理解题</span></a></h2>
+<p>先看问题再看选项，在文段中排除。从后往前看，出现代词往前看。</p>
+<p>同时，词句的理解有大小语境之分，小语境就是词句所在的那个句子，大语境就是词语所在的整个文段。分析语境可以先从小语境入手。</p>
+</div></template>
 
 
